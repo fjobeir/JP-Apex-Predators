0343b382d451de074a538e9a1a2e472437155e21
Feras Jobeir <fjobeir@gmail.com>
2023-06-30 01:26:20 +0300

    Initial Commit

---
+++ JP Apex Predators/JP_Apex_PredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JP_Apex_PredatorsApp.swift
+//  JP Apex Predators
+//
+//  Created by Feras Jobeir on 30.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct JP_Apex_PredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JP Apex PredatorsUITests/JP_Apex_PredatorsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  JP_Apex_PredatorsUITestsLaunchTests.swift
+//  JP Apex PredatorsUITests
+//
+//  Created by Feras Jobeir on 30.06.2023.
+//
+
+import XCTest
+
+final class JP_Apex_PredatorsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
