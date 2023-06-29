3d6a3770d69f02f181885299db82ac6a85f0e186
Feras Jobeir <fjobeir@gmail.com>
2023-06-30 01:36:47 +0300

    Apex Predator

---
+++ JP Apex Predators/ApexPredator.swift
@@ -0,0 +1,23 @@
+//
+//  ApexPredator.swift
+//  JP Apex Predators
+//
+//  Created by Feras Jobeir on 30.06.2023.
+//
+
+import Foundation
+
+struct ApexPredator: Codable {
+    let id: Int
+    let name: String
+    let type: String
+    let movies: [String]
+    let movieScenes: [MovieScene]
+    let link: String
+}
+
+struct MovieScene: Codable {
+    let id: Int
+    let movie: String
+    let sceneDescription: String
+}
